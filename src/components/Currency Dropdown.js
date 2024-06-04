c621738a2946a25b146f5d1bca27aa080c35a1b1
saurabhp94 <166574015+saurabhp94@users.noreply.github.com>
2024-06-03 20:35:17 -0400

    styles added

--- src/components/Currency Dropdown.js
+++ src/components/Currency Dropdown.js
@@ -1,38 +1,33 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import './Currency.css'; // Import the CSS file
 
 const Currency = () => {
-    const { currency, dispatch } = useContext(AppContext);
+  const { currency, dispatch, selectedCurrency } = useContext(AppContext);
 
-    
+  const handleCurrency = (e) => {
+    dispatch({
+      type: 'CHG_CURRENCY',
+      payload: e.target.value,
+    });
+  };
 
-    const handleCurrency = (e) => {
-        dispatch({
-            type: 'CHG_CURRENCY',
-            payload: e.target.value,
-        });
-    };
-
-    return (
-        <div className='alert alert-secondary'>
-        <select
-            onChange={handleCurrency}
-            className='form-select'
-            style={
-                {
-                    backgroundColor: "lightgreen",
-                }
-            }
-        >
-            {currency.map((option) => (
-                <option key={option} value={option}
-                style={{ backgroundColor: "lightgreen", hover: "green"}}>
-                    {option}
-                </option>
-            ))}
-        </select>
-        </div>
-    );
+  return (
+    <div className="currency-container">
+      <select
+        onChange={handleCurrency}
+        className="currency-select"
+        value={selectedCurrency}
+      >
+        {currency.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <div className="currency-arrow">▼</div>
+    </div>
+  );
 };
 
 export default Currency;
